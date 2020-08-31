1f9e05bbdc6f0ff95844b0f47e23d0f05c2fa399
Kamil Stukus <kamistukus.kontakt@gmail.com>
2020-08-31 20:52:27 +0200

    Add StyledWrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: pink;
+  position: relative;
+
+  ::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50px;
+    height: 50px;
+    display: block;
+    background: blue;
+  }
+`;
 
 const IndexPage = () => (
-  <div>
+  <StyledWrapper>
     <h1>Hi people</h1>
-  </div>
+  </StyledWrapper>
 );
 
 export default IndexPage;
